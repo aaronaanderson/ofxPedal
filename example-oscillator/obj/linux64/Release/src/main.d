b4734d53df7ad9eb33b9582dc6023e88c8353dc4
aaronaanderson <aaron.arthur.anderson@gmail.com>
2020-02-25 10:41:54 -0800

    deleted extra extern

--- example-oscillator/obj/linux64/Release/src/main.d
+++ example-oscillator/obj/linux64/Release/src/main.d
@@ -560,7 +560,7 @@ obj/linux64/Release/src/main.o: \
  /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/Interpolation.hpp \
  /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/Buffer.hpp \
  /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/TSine.hpp \
- /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/../../external/dr_wav.h \
+ /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/../../external/dr_wav.h \
  /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/Biquad.hpp \
  /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/Buffer.hpp \
  /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/BufferPlayer.hpp \
@@ -1751,7 +1751,7 @@ obj/linux64/Release/src/main.o: \
 
 /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/TSine.hpp:
 
-/home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/../../external/dr_wav.h:
+/home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/../../external/dr_wav.h:
 
 /home/anderson/Documents/of_v0.11.0_linux64gcc6_release/addons/ofxPedal/libs/Pedal/include/pedal/Biquad.hpp:
 
